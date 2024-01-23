9ab34d0618ee9b4e52af8cabfc702fb738e6f9de
BracketProto <69521768+OCSYT@users.noreply.github.com>
2024-01-23 14:53:12 +0000

    Update script.js

--- tracker/script.js
+++ tracker/script.js
@@ -194,6 +194,8 @@ async function connectToDevice() {
                         },
                         battery: battery
                     };
+
+                    
                     if (postDataCurrent == null) {
                         postDataCurrent = postData;
                     }
