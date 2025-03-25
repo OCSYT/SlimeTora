6ae0451c1417a7e881209590e4dd0657c776107e
Maya <jovannmc@femboyfurry.net>
2025-03-25 23:16:21 +0300

    chore: layout of project, fix issues
    tauri didnt like the icons (used tauri icon instead) and forgor to remove the non-existent dialog perm

---
+++ src/routes/(main)/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+    import "../app.css";
+    import "@fontsource/chakra-petch/index";
+
+    let { children } = $props();
+</script>
+
+{@render children()}
---
+++ src/routes/(main)/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  
+</script>
+
+<div class="text-center text-2xl text-blue-500">
+  Hello, World!
+</div>
