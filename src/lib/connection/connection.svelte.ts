e81d36f36079447d43a0f2ef6317cfcf9b086bfa
JovannMC <jovannmc@femboyfurry.net>
2025-03-31 20:02:56 +0300

    chore: use tabs instead of spaces

--- src/lib/connection/connection.svelte.ts
+++ src/lib/connection/connection.svelte.ts
@@ -1,24 +1,24 @@
 export abstract class ConnectionMode {
-    public name: string = "Unknown";
+	public name: string = "Unknown";
 
-    public isActive: boolean = false;
+	public isActive: boolean = false;
 
-    public isAvailable: boolean = false;
+	public isAvailable: boolean = false;
 
-    public supportedTrackers: string[] = [];
+	public supportedTrackers: string[] = [];
 
-    public async startConnection(): Promise<void> {
-        console.error("startConnection not implemented");
-        return Promise.reject(new Error("startConnection not implemented"));
-    }
+	public async startConnection(): Promise<void> {
+		console.error("startConnection not implemented");
+		return Promise.reject(new Error("startConnection not implemented"));
+	}
 
-    public async stopConnection(): Promise<void> {
-        console.error("stopConnection not implemented");
-        return Promise.reject(new Error("stopConnection not implemented"));
-    }
+	public async stopConnection(): Promise<void> {
+		console.error("stopConnection not implemented");
+		return Promise.reject(new Error("stopConnection not implemented"));
+	}
 
-    public getDevices(): string[] {
-        console.error("getDevices not implemented");
-        return [];
-    }
+	public getDevices(): string[] {
+		console.error("getDevices not implemented");
+		return [];
+	}
 }
--- src/lib/store/index.ts
+++ src/lib/store/index.ts
@@ -1,47 +1,47 @@
-import { invoke } from '@tauri-apps/api/core';
-import { writable, type Writable } from 'svelte/store';
+import { invoke } from "@tauri-apps/api/core";
+import { writable, type Writable } from "svelte/store";
 
 export const isOn: Writable<Boolean | null> = writable(null);
 isOn.subscribe(async (value) => {
-    if (value) {
-        await invoke('start');
-    } else if (value === false) {
-        await invoke('stop');
-    }
+	if (value) {
+		await invoke("start");
+	} else if (value === false) {
+		await invoke("stop");
+	}
 });
 
-export const currentPath = writable('/');
+export const currentPath = writable("/");
 export const navLinks = [
-    {
-        name: "Home",
-        icon: "ri:home-4-fill",
-        link: "/",
-    },
-    {
-        name: "Trackers",
-        icon: "ri:gps-line",
-        link: "/trackers",
-    },
-    {
-        name: "Settings",
-        icon: "ri:equalizer-line",
-        link: "/settings",
-    },
-    {
-        name: "About",
-        icon: "ri:information-line",
-        link: "/about",
-    },
+	{
+		name: "Home",
+		icon: "ri:home-4-fill",
+		link: "/",
+	},
+	{
+		name: "Trackers",
+		icon: "ri:gps-line",
+		link: "/trackers",
+	},
+	{
+		name: "Settings",
+		icon: "ri:equalizer-line",
+		link: "/settings",
+	},
+	{
+		name: "About",
+		icon: "ri:information-line",
+		link: "/about",
+	},
 ];
 export const externalNavLinks = [
-    {
-        name: "GitHub",
-        icon: "ri:github-line",
-        link: "https://github.com/OCSYT/SlimeTora",
-    },
-    {
-        name: "Docs",
-        icon: "ri:book-marked-line",
-        link: "https://github.com/OCSYT/SlimeTora/wiki",
-    },
+	{
+		name: "GitHub",
+		icon: "ri:github-line",
+		link: "https://github.com/OCSYT/SlimeTora",
+	},
+	{
+		name: "Docs",
+		icon: "ri:book-marked-line",
+		link: "https://github.com/OCSYT/SlimeTora/wiki",
+	},
 ];
