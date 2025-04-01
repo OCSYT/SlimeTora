0ebe63d7d67a32d46af97888f46f412793039c48
JovannMC <jovannmc@femboyfurry.net>
2025-04-01 21:41:13 +0300

    feat: crying 2 (use tauri-plugin-btleplug)

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,61 @@
+use std::{thread::sleep, time::{self, Duration}};
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tauri_plugin_btleplug::{
+    btleplug::{
+        api::{Central, Manager as _, Peripheral, ScanFilter},
+        platform::Manager,
+    },
+    BtleplugExt,
+};
 mod util;
 
 #[tauri::command]
-fn start() {
+async fn start(app_handle: tauri::AppHandle) -> Result<(), String> {
     util::log("Started connection");
+    let _ = app_handle
+        .btleplug()
+        .btleplug_context_spawn(async move {
+            let manager = Manager::new().await.map_err(|e| e.to_string())?;
+            let adapter_list = manager.adapters().await.map_err(|e| e.to_string())?;
+            if adapter_list.is_empty() {
+                return Err("No Bluetooth adapters found".to_string());
+            }
+            util::log(&format!(
+                "Found {} Bluetooth adapters",
+                adapter_list.len()
+            ));
+
+            for adapter in adapter_list {
+                let info = adapter.adapter_info().await.unwrap_or_else(|_| "Unknown".to_string());
+                util::log(&format!("Adapter: {}", info));
+                adapter
+                    .start_scan(ScanFilter::default())
+                    .await
+                    .expect("Failed to start scan");
+                sleep(Duration::from_secs(2));
+                let devices = adapter.peripherals().await.map_err(|e| e.to_string())?;
+                if devices.is_empty() {
+                    util::log("No devices found");
+                } else {
+                    for device in devices {
+                        let properties = device.properties().await.map_err(|e| e.to_string());
+                        let name = properties
+                            .and_then(|p| Ok(p.unwrap().local_name))
+                            .map(|n| n.unwrap_or_default());
+                        util::log(&format!(
+                            "Found Bluetooth device: {}",
+                            name.unwrap_or("Unknown".to_string())
+                        ));
+                    }
+                }
+            }
+
+            Ok(())
+        })
+        .await
+        .expect("error during btleplug task");
+    Ok(())
 }
 
 #[tauri::command]
@@ -17,7 +69,20 @@ pub fn run() {
         .plugin(tauri_plugin_prevent_default::init())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_serialplugin::init())
-        .plugin(tauri_plugin_blec::init())
+        .plugin(tauri_plugin_btleplug::init())
+        .setup(|app: &mut tauri::App| {
+            #[cfg(mobile)]
+            app.btleplug()
+                .request_permissions(tauri_plugin_btleplug::permission::RequestPermission {
+                    bluetooth: true,
+                    bluetooth_admin: true,
+                    bluetooth_advertise: true,
+                    bluetooth_connect: true,
+                    bluetooth_scan: true,
+                })
+                .expect("error while requesting permissions");
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![start, stop])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/lib/backend/connection/ble.svelte.ts
+++ src/lib/backend/connection/ble.svelte.ts
@@ -2,6 +2,9 @@ import { ConnectionMode } from "./connection.svelte";
 import { type BleDevice } from "@mnlphlp/plugin-blec";
 import * as ble from "@mnlphlp/plugin-blec";
 
+// ok so https://github.com/MnlPhlp/tauri-plugin-blec doesn't support multiple devices. bruh.
+// gonna try this repo lol (w/ btleplug): https://github.com/Dreaming-Codes/tauri-plugin-btleplug
+
 export class BLE extends ConnectionMode {
 	public name: string = "Bluetooth";
 	public isActive: boolean = false;
@@ -12,61 +15,15 @@ export class BLE extends ConnectionMode {
 
 	constructor() {
 		super();
-
-		// Check if Bluetooth is available
-		ble.checkPermissions()
-			.then((result) => {
-				this.isAvailable = result;
-				if (this.isAvailable) {
-					console.log("Bluetooth is available");
-				} else {
-					console.warn("Bluetooth is not available");
-				}
-			})
-			.catch((error) => {
-				this.isAvailable = false;
-				console.error(`Error checking Bluetooth permissions: ${error}`);
-			});
 	}
 
 	public async startConnection() {
-		if (!this.isAvailable) {
-			const msg = "Bluetooth is not available";
-			console.error(msg);
-			return Promise.reject(new Error(msg));
-		}
-
-		this.isActive = true;
-
-		try {
-			// TODO: keep scanning for devices until stopped
-			await ble.startScan(this.handleDiscoveredDevice, 5000);
-			console.log("Bluetooth scan started");
-		} catch (error) {
-			this.isActive = false;
-			console.error(`Failed to start Bluetooth scan: ${error}`);
-			return Promise.reject(error);
-		}
+		console.error("startConnection not implemented");
+		return Promise.reject(new Error("startConnection not implemented"));
 	}
 
 	public async stopConnection() {
 		console.error("stopConnection not implemented");
 		return Promise.reject(new Error("stopConnection not implemented"));
-	}
-
-	private handleDiscoveredDevice(devices: BleDevice[]) {
-		console.log(`Discovered ${devices.length} devices`);
-		console.log(`Devices: ${JSON.stringify(devices)}`);
-		devices.forEach((device) => {
-			if (device.name.startsWith("Haritora")) {
-				console.log(`Found Haritora device: ${device.name}`);
-				ble.connect(device.address, () => {
-					console.log(`Disconnected from ${device.name}`);
-				});
-				ble.subscribeString
-				this.activeDevices.push(device);
-			}
-		});
-	}
-	
+	}	
 }
