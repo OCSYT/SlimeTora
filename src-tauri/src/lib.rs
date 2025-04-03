851ae7949365dfd7d6ddcecf5c27843a2d9b7b59
JovannMC <jovannmc@femboyfurry.net>
2025-04-03 10:18:41 +0300

    fix: find names of devices on DeviceUpdated event

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -21,29 +21,56 @@ async fn start(app_handle: tauri::AppHandle) -> Result<(), String> {
         .btleplug()
         .btleplug_context_spawn(async move {
             let manager = Manager::new().await.map_err(|e| e.to_string())?;
-            let adapters = manager.adapters().await.unwrap();
+            let adapters = manager.adapters().await.map_err(|e| e.to_string())?;
             if adapters.is_empty() {
                 return Err("No Bluetooth adapters found".to_string());
             }
             util::log(&format!("Found {} Bluetooth adapters", adapters.len()));
 
             let central = adapters.into_iter().nth(0).unwrap();
-            let central_state = central.adapter_state().await.unwrap();
+            let central_state = central.adapter_state().await.map_err(|e| e.to_string())?;
             println!("Central state: {:?}", central_state);
 
-            let mut events = central.events().await.unwrap();
+            let mut events = central.events().await.map_err(|e| e.to_string())?;
 
-            let _ = central.start_scan(ScanFilter::default()).await;
+            let _ = central
+                .start_scan(ScanFilter::default())
+                .await
+                .map_err(|e| e.to_string())?;
 
             while let Some(event) = events.next().await {
                 match event {
                     CentralEvent::DeviceDiscovered(device) => {
-                        let peripheral = central.peripheral(&device).await.unwrap();
-                        let name = peripheral.properties().await.unwrap().unwrap().local_name;
+                        let peripheral = central
+                            .peripheral(&device)
+                            .await
+                            .map_err(|e| e.to_string())?;
+                        let properties = peripheral
+                            .properties()
+                            .await
+                            .map_err(|e| e.to_string())
+                            .clone()?;
+                        peripheral.is_connected().await.map_err(|e| e.to_string())?;
+
+                        let name = properties.as_ref().and_then(|p| p.local_name.clone());
                         if let Some(name) = name {
-                            util::log(&format!("Discovered device: {}", name));
-                        } else {
-                            util::log("Discovered device with no name");
+                            // TODO: pass to a function that checks name, and if it matches, connect to the device
+                            println!("Device discovered: {:?}", name);
+                        }
+                    }
+                    CentralEvent::DeviceUpdated(id) => {
+                        println!("DeviceUpdated: {:?}", id);
+                        let peripheral =
+                            central.peripheral(&id).await.map_err(|e| e.to_string())?;
+                        let properties = peripheral
+                            .properties()
+                            .await
+                            .map_err(|e| e.to_string())
+                            .clone()?;
+                        let name = properties.as_ref().and_then(|p| p.local_name.clone());
+                        if let Some(name) = name {
+                            // TODO: pass to a function that checks name, and if it matches, connect to the device
+                            println!("Device updated: {:?}", name);
                         }
                     }
                     CentralEvent::StateUpdate(state) => {
